813ce13f33c4049c3ac42f357fa47e318f88f507
supravi96 <supravi96@gmail.com>
2019-10-07 18:58:52 +0530

    inital commit

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Ingredient } from '../shared/ingredients.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple',12),
+    new Ingredient('Orange',10)
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
